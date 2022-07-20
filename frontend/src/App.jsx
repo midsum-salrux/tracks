7b1fe29324f9eb987c84afdbbeee180938a66c84
midsum-salrux <idempotato@protonmail.com>
2022-07-20 13:09:08 -0400

    Frontend scry

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Urbit from "@urbit/http-api";
 
 export default function App() {
+  const [tracks, setTracks] = useState([]);
+
+  async function tracksScry() {
+    const result = await window.urbit.scry({app: "tracks", path: "/tracks"});
+    setTracks(result);
+  }
+
   window.urbit = new Urbit("");
   window.urbit.ship = window.ship;
 
+  useEffect(tracksScry, []);
+
   return <>
-    <h1>Tracks</h1>
+    <code>{JSON.stringify(tracks)}</code>
   </>;
 }
