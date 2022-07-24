1dfdd361c0d09d8bc9fd13929e91b1b280d3bc3c
midsum-salrux <idempotato@protonmail.com>
2022-07-23 22:27:02 -0400

    Move buttons now work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,34 @@ export default function App() {
   const [tracks, setTracks] = useState([]);
 
   async function tracksScry() {
-    const result = await window.urbit.scry({app: "tracks", path: "/tracks"});
-    setTracks(result);
+    const scryResult = await window.urbit.scry({app: "tracks", path: "/tracks"});
+
+    const tracks = scryResult.map(function(track) {
+      let newTrack = track;
+
+      // because millisecond timestamps are serialized with scientific notation
+      // which the backend can't parse correctly, we need to turn the timestamps
+      // into second timestamps
+      newTrack.results = track.results.map(function(result) {
+        let newResult = result;
+
+        if ("ud" in result) {
+          newResult.ud.time = (result.ud.time / 1000)
+        } else if ("rd" in result) {
+          newResult.rd.time = (result.rd.time / 1000)
+        } else if ("bool" in result) {
+          newResult.bool.time = (result.bool.time / 1000)
+        } else if ("tape" in result) {
+          newResult.tape.time = (result.tape.time / 1000)
+        }
+
+        return newResult;
+      });
+
+      return newTrack;
+    })
+
+    setTracks(tracks);
   }
 
   function changeTracksPoke(newTracks) {
