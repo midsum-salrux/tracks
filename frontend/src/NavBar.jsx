f4dd098e2ce04f35f5d951258eac58df1aaad5c5
midsum-salrux <idempotato@protonmail.com>
2022-07-30 19:04:01 -0400

    Nav bar tweaks

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { PlusIcon } from "@primer/octicons-react";
+
 
 export default function NavBar(props) {
   return <>
@@ -22,11 +24,11 @@ export default function NavBar(props) {
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-4 bg-white border-bottom box-shadow">
       <h5 className="my-0 mr-md-auto font-weight-normal">Tracks</h5>
       <nav className="my-2 my-md-0 mr-md-3">
-        <a className="badge badge-light mr-3" href="#">
-          New Track
-        </a>
-        <a className="badge badge-light" href="#"
-           data-toggle="modal" data-target="#about">?</a>
+        <button className="btn btn-light btn-sm mr-3" href="#">
+          <PlusIcon />
+        </button>
+        <button className="btn btn-light btn-sm" href="#"
+           data-toggle="modal" data-target="#about">?</button>
       </nav>
     </div>
   </>
